c0cd685db252d1b880c465c44943effdeebc56f7
corybitner <cory@corybitner.com>
2025-08-04 00:15:50 -0600

    Delete includes directory

--- includes/notice.php
+++
@@ -1,9 +0,0 @@
-<?php if(isset($msg)) : ?>
-  	<div class="updated notice is-dismissible"> 
-		<p><strong><?php echo($msg); ?></strong></p>
-	</div>
-<?php elseif(isset($msge)) : ?>	
-	<div class="error notice is-dismissible">
-	    <p><strong><?php echo($msge); ?></strong></p>
-	</div>
-<?php endif; ?>
